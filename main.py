89641c99cec212ef8e85a74f4e29b8f4061ce1e8
Daven <davenrai@gmail.com>
2020-10-07 15:12:45 -0400

    Refactored code. Included error-checking and continuation.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import tweepy
 import requests
 import datetime
+from tweepy import *
 from random import randint
 from time import sleep
+
+from urllib3.exceptions import ReadTimeoutError
+
 from Confidential import *
 
 URL = "https://api.kanye.rest/"
+
 
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, api):
@@ -14,7 +19,7 @@ class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         if "@DavenBot" in status.text and "#kanye" in status.text \
                 and status.author.screen_name != "DavenBot":
-            result = run(api, status)
+            run(api, status)
 
 
 def get_twitter_api():
@@ -22,10 +27,10 @@ def get_twitter_api():
     Returns Twitter API.
     :return:
     """
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    bot_auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
+    bot_auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    return tweepy.API(auth, wait_on_rate_limit=True)
+    return tweepy.API(bot_auth, wait_on_rate_limit=True)
 
 
 def get_kanye_quote():
@@ -43,29 +48,39 @@ def run(api: tweepy.API, tweet=None):
     quote = get_kanye_quote()
 
     output_tweet = quote + " -Kanye West"
-    if tweet is not None:
-        output_tweet = "@%s %s" % (tweet.author.screen_name, output_tweet)
-        result = api.update_status(status=output_tweet,
-                                   in_reply_to_status_id=tweet.id)
-    else:
-        result = api.update_status(status=output_tweet)
-    print("Posted to Twitter")
-    return result
+    try:
+        if tweet is not None:
+            output_tweet = "@%s %s" % (tweet.author.screen_name, output_tweet)
+            result = api.update_status(status=output_tweet,
+                                       in_reply_to_status_id=tweet.id)
+        else:
+            result = api.update_status(status=output_tweet)
+        print("Posted to Twitter")
+        return result
+    except Exception:
+        print('Could not post Tweet.')
+        pass
 
 
 if __name__ == '__main__':
-    print("Getting Twitter API")
-    api = get_twitter_api()
-    myStreamListener = MyStreamListener(api)
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter(track=["@DavenBot", "#kanye", "@davenbot"], is_async=True)
+    print("Starting Kanye Bot.")
+    while True:  # Infinite loop so a mention to bot isn't required to tweet
+        try:
+            api = get_twitter_api()
+            myStreamListener = MyStreamListener(api)
+            myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+            myStream.filter(track=["@DavenBot", "#kanye", "@davenbot"],
+                            is_async=True)
+            run(api)
 
-    while True: # Infinite loop so a mention to bot isn't required to tweet
-        run(api)
-        random_int = randint(3600, 21600) # tweets randomly between 1 - 6 hrs
-        time = datetime.timedelta(seconds=random_int)
-        print("Waiting for " + str(time))
-        sleep(random_int)
+            # tweets randomly between 1 - 6 hrs
+            random_int = randint(3600, 21600)
 
-
-
+            time = datetime.timedelta(seconds=random_int)
+            print("Waiting for " + str(time))
+            sleep(random_int)
+        except (TweepError, RuntimeError, ConnectionError, ReadTimeoutError) \
+                as e:
+            print("Restarting Stream.")
+            sleep(500)
+            continue
